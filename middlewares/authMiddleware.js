b176802c71c63bf057d94f6adf68a3389084c11b
Masud Ahmed <ma7332125@gmail.com>
2024-12-01 04:21:15 +0530

    api limiter msg change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -20,7 +20,7 @@ export const otpRequestLimiter = rateLimit({
   max: 3, 
   message: {
     success: false,
-    message: 'Too many OTP requests, please try again after 24 hours'
+    message: 'Too many requests, please try again after 24 hours'
   },
   standardHeaders: true, 
   legacyHeaders: false,  
