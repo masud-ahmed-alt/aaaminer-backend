0bc66f9245be6ffd09c518b67ab6d04edf99ffaf
Masud Ahmed <ma7332125@gmail.com>
2024-12-01 04:17:44 +0530

    some apis limit set

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -17,7 +17,7 @@ export const isAuthenticated = (req, resp, next) => {
 
 export const otpRequestLimiter = rateLimit({
   windowMs: 1440 * 60 * 1000, 
-  max: 500, 
+  max: 3, 
   message: {
     success: false,
     message: 'Too many OTP requests, please try again after 24 hours'
