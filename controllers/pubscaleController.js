0ffc728b87cd22af5516a33e4a5ef3d78bb11b93
Masud Ahmed <ma7332125@gmail.com>
2025-07-31 01:14:52 +0530

    pubscale s2s testing

--- controllers/pubscaleController.js
+++ controllers/pubscaleController.js
@@ -3,8 +3,6 @@ import crypto from "crypto";
 import { catchAsyncError } from "../middlewares/errorMiddleware.js";
 import { ErrorHandler } from "../utils/utility.js";
 
-
-
 export const handleCallback = catchAsyncError(async (req, res, next) => {
   const { user_id, value, token, signature } = req.query;
 
@@ -12,7 +10,10 @@ export const handleCallback = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Missing parameters", 400));
   }
 
-  // Generate and validate signature
+  console.log("Received callback with parameters:", {
+    value,
+  });
+
   const template = `${process.env.PUBSCALE_SECRET_KEY}.${user_id}.${Math.trunc(
     Number(value)
   )}.${token}`;
@@ -23,18 +24,16 @@ export const handleCallback = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Invalid signature", 403));
   }
 
-  // Treat user_id as username for lookup
   const updatedUser = await User.findOneAndUpdate(
     { username: user_id },
     { $inc: { walletPoints: parseInt(value, 10) } },
     { new: true }
   );
-  console.log("Updated User:", updatedUser);
+
   if (!updatedUser) {
     return next(new ErrorHandler("User not found", 404));
   }
 
-  // Successful update response
   res.status(200).json({
     success: true,
     message: "Wallet points updated successfully",
